3cded30c41de6d13aa972d6c0ae387e561f96264
jetteraw <Andy.Jetter@gmail.com>
2016-01-30 16:02:09 -0500

    Create Project
    rdydrtd

---
+++ GlobalGameJam/Assets/Scripts/monsterController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterController : MonoBehaviour {
+	
+	public int monsterType;
+	float monsterSpeed;
+	Vector2 monsterMovement;
+	Vector2 monsterRandX; //This vector 2 represents the range of randmness for movement in the x direction each time step
+	Vector2 monsterRandY; //This vector 2 represents the range of randomness for movement in the y direction each time step
+	float monsterPosX;
+	float monsterPosY;
+
+	// Use this for initialization
+	void Start () {
+
+		//change to 
+
+			monsterMovement = new Vector2(-1f,1f);
+			monsterRandX = new Vector2(-1f,1f);
+			monsterRandY = new Vector2(0f,1f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		monsterPosX = monsterPosX + (monsterSpeed + Random.Range (monsterRandX.x,monsterRandX.y)) * Time.deltaTime;
+		monsterPosY = monsterPosY + (monsterSpeed + Random.Range (monsterRandY.x,monsterRandY.y)) * Time.deltaTime;
+
+		transform.position = new Vector2(monsterPosX,monsterPosY);
+	}
+}
---
+++ GlobalGameJam/Assets/Scripts/monsterSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterSpawner : MonoBehaviour {
+
+	int queueLength = 100;
+	int[,] monsterTypeQueue;
+	public GameObject monster;
+	public int queueCounter;
+	bool isRoundActive ;
+
+	// Use this for initialization
+	void Start () { 
+		monsterTypeQueue = new int [,] { {0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0},{0,0,0,0,0,1,1,1,1,1,0,1,0,0,0,1,1,1,1,1},{0,2,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1},{1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,1,1,1,0,0} };
+		queueCounter = 0;
+		}
+	
+
+
+		
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
